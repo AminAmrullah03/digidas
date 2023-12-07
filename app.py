bb77f5aee5e1bf51c9a4d94162d2345c4a5c73ca
Alfin Gunawan <alfingunawan003@gmail.com>
2023-12-07 19:32:45 +0700

    Pembuatan landingpage, style & kode service

--- app.py
+++ app.py
@@ -1,2 +1,14 @@
-import flask 
-import pymongo
+from flask import Flask,redirect,url_for,render_template,request
+
+app=Flask(__name__)
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/register.html')
+def register():
+    return render_template('register.html')
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0',port=5000,debug=True)
