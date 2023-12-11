7332b321185606973474de0aa8c7ce95fe139e12
AminAmrullah03 <vickyblendes25@gmail.com>
2023-12-12 06:21:14 +0700

    mutasi backend

--- app.py
+++ app.py
@@ -17,6 +17,7 @@ DB_NAME = os.environ.get("DB_NAME")
 
 client = MongoClient(MONGODB_URI)
 db = client[DB_NAME]
+pindahan = db['pindahan']
 pelanggarans = db['pelanggarans']
 datasantri = db['datasantri']
 datainv = db['datainv']
@@ -174,7 +175,20 @@ def edit_data_inventaris(id):
 
 @app.route('/mutasi')
 def mutasi():
-    return render_template('mutasi.html')
+    data = pindahan.find()
+    return render_template('mutasi.html', data=data)
+
+@app.route('/tambah_mutasi', methods=['POST'])
+def tambah_mutasi():
+    if request.method == 'POST':
+        data = {
+            'nama': request.form['nama'],
+            'jenis_kelamin': request.form['jenis_kelamin'],
+            'tanggal': request.form['tanggal'],
+            'status': request.form['status'],
+        }
+        pindahan.insert_one(data)
+        return redirect(url_for('mutasi', success=1))
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
